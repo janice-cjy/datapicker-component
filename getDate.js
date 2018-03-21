c6243e1e150721ec85a28085056c7f9a0df3bac5
Janice Chen <1027231121@qq.com>
2018-03-21 23:54:25 +0800

    creat total html and dill data

---
+++ getDate.js
@@ -0,0 +1,63 @@
+(function() {
+	var datePicker = {};
+
+	datePicker.getDate = function (year,month) {
+		var dateArr = [];
+		var thisYear,thisMonth;
+		var preMonthDays;   //上个月包含的天数
+		var firstDayOfMonth;  //这个月第一天的为周几
+		var lastDateOfLastMonth;  //上个月最后一天的日期
+		var lastDateOfMonth;  //这个月最后一天的日期
+
+		if (!year||!month) {
+			thisYear = new Date().getFullYear();
+			thisMonth = new Date().getMonth() + 1;
+		}
+		firstDayOfMonth = new Date(thisYear,thisMonth - 1,1).getDay();
+		if (firstDayOfMonth == 0) {
+			firstDayOfMonth = 7;
+		}
+		preMonthDays = firstDayOfMonth - 1;
+
+		lastDateOfLastMonth = new Date(thisYear,thisMonth - 1,0).getDate();
+
+		lastDateOfMonth = new Date(thisYear,thisMonth,0).getDate();
+		var markData;
+		var year,month;
+		for(var i = 0;i < 5*7;i++){
+			markData = i - preMonthDays + 1;
+			month = thisMonth;
+			var date = markData;
+			if (markData <=0) {
+				date = lastDateOfLastMonth + markData;
+				month = thisMonth - 1;
+			}
+			if (markData > lastDateOfMonth) {
+				date = markData - lastDateOfMonth;
+				month = thisMonth + 1;
+			}
+			
+			if(month == 0) {
+				month = 12;
+				year = thisYear - 1;
+			}else if (month == 13) {
+				month = 1;
+				year = thisYear + 1;
+			}else {
+				year = thisYear;
+			}
+
+			dateArr.push({
+				markData:markData,
+				date:date,
+				year:year,
+				month:month
+			})
+
+
+		}
+		return dateArr;
+	}
+
+	window.datePicker = datePicker;
+})()
